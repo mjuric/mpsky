9e05c995cf0ab9ae42eb03ef35e289a4d670b8b4
Mario Juric <mjuric@astro.washington.edu>
2026-01-19 16:32:41 +0100

    docker/entrypoint.sh: simplify

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-DATASTORE=${CACHEURL:="https://epyc.astro.washington.edu/~mjuric/mpsky-data"}
-MAX_LOADED_NIGHTS=7
-MAX_ONDISK_NIGHTS=3
-
-exec mpsky serve \
-	--cache-datastore "$DATASTORE" \
-	--max-loaded-nights "$MAX_LOADED_NIGHTS" \
-	--max-ondisk-nights "$MAX_ONDISK_NIGHTS" \
-	${@}
+exec mpsky ${@}
